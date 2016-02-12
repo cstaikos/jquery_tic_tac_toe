f842393323dc2469249c335bef5f47813f975b60
Chris Staikos <cstaikos@gmail.com>
2016-02-12 12:14:47 -0500

    Fix bug where game would start at player O after the first game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).on('ready', function() {
-  new game().init();
+  var currentGame = new game();
+  currentGame.init();
 });
 
 function game() {
@@ -19,40 +20,42 @@ function game() {
   }
 
   this.attachHandlers = function() {
-    $(".container").on( "click", ".open", this, this.makeMove );
+    $(".container").off().on( "click", ".open", this, this.makeMove );
   }
 
   this.gameState = function() {
-  var winningSequences = ["#a1, #a2, #a3",
-                      "#b1, #b2, #b3",
-                      "#c1, #c2, #c3",
-                      "#a1, #b1, #c1",
-                      "#a2, #b2, #c2",
-                      "#a3, #b3, #c3",
-                      "#a1, #b2, #c3",
-                      "#a3, #b2, #c1"]
-  var winningString = this.currentPlayer + this.currentPlayer + this.currentPlayer;
-  var gameWon = false;
+    var winningSequences = ["#a1, #a2, #a3",
+                            "#b1, #b2, #b3",
+                            "#c1, #c2, #c3",
+                            "#a1, #b1, #c1",
+                            "#a2, #b2, #c2",
+                            "#a3, #b3, #c3",
+                            "#a1, #b2, #c3",
+                            "#a3, #b2, #c1"]
+    var winningString = this.currentPlayer + this.currentPlayer + this.currentPlayer;
+    var gameWon = false;
 
-  for (i=0; i<winningSequences.length; i++) {
-    if ($(winningSequences[i]).text() === winningString) {
-      gameWon = true;
+    for (i=0; i<winningSequences.length; i++) {
+      if ($(winningSequences[i]).text() === winningString) {
+        gameWon = true;
+      }
     }
-  }
 
-  if (!gameWon) {
-    return ($(".open").length === 0 ? 0 : null);
-  }
-  else {
-    return (this.currentPlayer === "X" ? 1 : -1);
-  }
+    if (!gameWon) {
+      return ($(".open").length === 0 ? 0 : null);
+    }
+    else {
+      return (this.currentPlayer === "X" ? 1 : -1);
+    }
 
-  }
+  }// gameState
 
   this.makeMove = function(arg) {
     $(this).text(arg.data.currentPlayer).removeClass("open");
+    if (arg.data.updateGameFlow()) {
+      return; //Skip toggling the player and simply return if the game is finished (avoids toggling to "O" when next game starts)
+    }
     arg.data.togglePlayer();
-    arg.data.updateGameFlow();
   }
 
   this.togglePlayer = function() {
@@ -60,47 +63,32 @@ function game() {
   }
 
   this.updateGameFlow = function() {
-    if (this.gameState() === null) return;
-      var newMessage = "";
+    if (this.gameState() === null) return; //If game is still going we can ignore the rest of this function
 
-      switch (this.gameState()) {
-        case 1 : // X wins
-          newMessage = "X wins! ";
-          break;
-        case -1 : // O wins
-          newMessage = "O wins! ";
-          break;
-        case 0 : // tie game
-          newMessage = "Tie Game! ";
-          break;
-        } //switch
+    var newMessage = "";
 
-        newMessage += "Click anywhere to start a new game.";
-        this.updateMessage(newMessage);
+    switch (this.gameState()) {
+      case 1 : // X wins
+        newMessage = "X wins! ";
+        break;
+      case -1 : // O wins
+        newMessage = "O wins! ";
+        break;
+      case 0 : // tie game
+        newMessage = "Tie Game! ";
+        break;
+      } //switch
 
-      } //updateGameFlow
+      newMessage += "Click anywhere to start a new game.";
+      this.updateMessage(newMessage);
 
+      $(".open").removeClass("open");
+      alert("Would you like to play again?");
+      this.init();
+
+      return true;
+
+
+    } //updateGameFlow
 
   }//game class
-
-
-
-// var initGame = function() {
-//   $("#message").text("Welcome to jQuery tic-tac-toe! Click a square to begin:");
-//   $(".square").addClass("open");
-// }
-
-// var makeMove = function() {
-//   $(this).text(currentPlayer).removeClass("open");
-//
-//   currentPlayer = (currentPlayer === "X" ? "O" : "X");
-//
-//   if (gameState() !== null) {
-//     $("#message").text(gameState() + " Click anywhere to start another.");
-//   }
-// }
-
-// Returns the game state as a message...if the game is not over returns null
-// var gameState = function() {
-//   return ($(".open").length === 0 ? "Tie Game!" : null);
-// }
