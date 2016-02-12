b98fdce675c3b1db081b43fbaf4416901911fb28
Chris Staikos <cstaikos@gmail.com>
2016-02-11 22:24:18 -0500

    Get basic game function working, game state detected

--- script.js
+++ script.js
@@ -1,45 +1,89 @@
 $(document).on('ready', function() {
-  // $(".container").on( "click", ".open", function() {
-  //     makeMove();
-  // });
-
-  var ticTacToe = new game();
-  ticTacToe.init();
-
+  new game().init();
 });
-
 
 function game() {
 
   this.init = function() {
     $(".square").addClass("open").text("");
+
     this.currentPlayer = "X";
-    this.message = "Welcome to jQuery tic-tac-toe! Click a square to begin:";
-    this.updateMessage();
+
+    this.updateMessage("Welcome to jQuery tic-tac-toe! Click a square to begin:");
+    this.attachHandlers();
   }
 
-  this.updateMessage = function() {
+  this.updateMessage = function(newMessage) {
+    this.message = newMessage;
     $("#message").text(this.message);
   }
 
-  this.gameState = function() {
-    var newMessage = "";
-    newMessage = ($(".open").length === 0 ? "Tie Game!" : null);
-
-    return newMessage + " Click anywhere to start another.";
+  this.attachHandlers = function() {
+    $(".container").on( "click", ".open", this, this.makeMove );
   }
 
-  this.makeMove = function(element) {
-    $(element).text(currentPlayer).removeClass("open");
+  this.gameState = function() {
+  var winningSequences = ["#a1, #a2, #a3",
+                      "#b1, #b2, #b3",
+                      "#c1, #c2, #c3",
+                      "#a1, #b1, #c1",
+                      "#a2, #b2, #c2",
+                      "#a3, #b3, #c3",
+                      "#a1, #b2, #c3",
+                      "#a3, #b2, #c1"]
+  var winningString = this.currentPlayer + this.currentPlayer + this.currentPlayer;
+  var gameWon = false;
 
-    currentPlayer = (currentPlayer === "X" ? "O" : "X");
-
-    if (gameState() !== null) {
-      updateMessage( gameState() );
+  for (i=0; i<winningSequences.length; i++) {
+    if ($(winningSequences[i]).text() === winningString) {
+      gameWon = true;
     }
   }
 
-}
+  if (!gameWon) {
+    return ($(".open").length === 0 ? 0 : null);
+  }
+  else {
+    return (this.currentPlayer === "X" ? 1 : -1);
+  }
+
+  }
+
+  this.makeMove = function(arg) {
+    $(this).text(arg.data.currentPlayer).removeClass("open");
+    arg.data.togglePlayer();
+    arg.data.updateGameFlow();
+  }
+
+  this.togglePlayer = function() {
+    this.currentPlayer = (this.currentPlayer === "X" ? "O" : "X");
+  }
+
+  this.updateGameFlow = function() {
+    if (this.gameState() === null) return;
+      var newMessage = "";
+
+      switch (this.gameState()) {
+        case 1 : // X wins
+          newMessage = "X wins! ";
+          break;
+        case -1 : // O wins
+          newMessage = "O wins! ";
+          break;
+        case 0 : // tie game
+          newMessage = "Tie Game! ";
+          break;
+        } //switch
+
+        newMessage += "Click anywhere to start a new game.";
+        this.updateMessage(newMessage);
+
+      } //updateGameFlow
+
+
+  }//game class
+
+
 
 // var initGame = function() {
 //   $("#message").text("Welcome to jQuery tic-tac-toe! Click a square to begin:");
